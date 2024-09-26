f6fff2399acf73dec8516163100b2b6d05f4b3cc
Vadym <vadymzastavniy@gmail.com>
2024-09-26 16:46:36 +0300

    add react router dom

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PricingToggle from './components/PricingToggle';
 import PricingPlans from './components/PricingPlans';
 import BgLeft from "./assets/images/bg-left.svg";
 import BgRight from "./assets/images/bg-right.svg";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
     const [isMonthly, setIsMonthly] = useState(false);
@@ -13,15 +14,17 @@ function App() {
         setIsMonthly(toggleState);
     };
   return (
-      <div className="app">
-        <Header />
-        <main>
-            <PricingToggle onToggle={handleToggle} />
-            <PricingPlans isMonthly={isMonthly} />
-        </main>
-        <img src={BgLeft} className="bg-left" alt="Left background element" />
-        <img src={BgRight} className="bg-right" alt="Right background element" />
-      </div>
+      <Router basename={process.env.PUBLIC_URL}>
+          <div className="app">
+            <Header />
+            <main>
+                <PricingToggle onToggle={handleToggle} />
+                <PricingPlans isMonthly={isMonthly} />
+            </main>
+            <img src={BgLeft} className="bg-left" alt="Left background element" />
+            <img src={BgRight} className="bg-right" alt="Right background element" />
+          </div>
+      </Router>
   );
 }
 
